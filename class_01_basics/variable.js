3e0dd5c47c5da7109ba7282e1c8feeefe788f21f
Hassaan Mahmood <hassaan2980@gmail.com>
2026-06-25 22:04:57 +0000

    defined array

--- class_01_basics/variable.js
+++ class_01_basics/variable.js
@@ -1,19 +1,27 @@
-const accountID = 1122;
-131415161718
+// const accountID = 1122;
+// 131415161718
 
-let accountName = "Hassaan";
-var accountPassword = 1234;
-accountCity = "Karachi";
-// accountState;
+// let accountName = "Hassaan";
+// var accountPassword = 1234;
+// accountCity = "Karachi";
+// // accountState;
 
+// accountId = 810011;
+// accountName = "Hassaan Mahmood";
+// accountPassword = 4444;
+// accountCity = "sukkur";
 
-accountId = 810011;
-accountName = "Hassaan Mahmood";
-accountPassword = 4444;
-accountCity = "sukkur";
+// // console.log(accountName);
+// console.table([accountID,accountName,accountPassword,accountCity])
 
+let cities = ["Karachi, Hyderabad, Sukkur, Peshawar, Islamabad"];
+// isFound = false;
 
-// console.log(accountName);
-console.table([accountID,accountName,accountPassword,accountCity])
-
-6789101134512
+for (let index = 0; index < cities.length; index++) {
+  if (cities[index] === "Karachi") {
+    console.log("It is one of the cleanest city");
+  } else {
+    console.log("It is not of the cleanest city");
+  }
+  console.log(cities[index]);
+}
