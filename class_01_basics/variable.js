4470aee0f0e56679e4c561464d143037766a9d13
Hassaan Mahmood <hassaan2980@gmail.com>
2026-06-25 21:33:06 +0000

    Variables defined and have executed them

---
+++ class_01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountID = 1122;
+131415161718
+
+let accountName = "Hassaan";
+var accountPassword = 1234;
+accountCity = "Karachi";
+// accountState;
+
+
+accountId = 810011;
+accountName = "Hassaan Mahmood";
+accountPassword = 4444;
+accountCity = "sukkur";
+
+
+// console.log(accountName);
+console.table([accountID,accountName,accountPassword,accountCity])
+
+6789101134512
--- class_03/app.js
+++
@@ -1,9 +0,0 @@
-console.log("Hassaan")
-console.log("Hello I am Hassaan Mahmood");
-
-
-
-
-for (var i = 0; i <= 10; i++ ){
-    console.log(i);
-}
