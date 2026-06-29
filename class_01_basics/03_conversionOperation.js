9e8a0d90566ffc920f3ee5f44010fe0ec82618a5
Hassaan Mahmood <hassaan2980@gmail.com>
2026-06-29 13:41:12 -0700

    Enhance conversionOperation.js with examples
    Updated comments and added new examples for type conversion and arithmetic operations.

--- class_01_basics/03_conversionOperation.js
+++ class_01_basics/03_conversionOperation.js
@@ -11,9 +11,9 @@ console.log(typeof(score));
 let valueInNumer = Number(score);
 console.log(valueInNumer);
 
-// "123" => 123
-// "123abc" => NaN
-// true => 1, false => 0
+"123" => 123
+"123abc" => NaN
+true => 1, false => 0
 
 
 let isLoggedIn = "Hassaan"; // true
@@ -31,3 +31,30 @@ let someNumber = 25;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let str1 = "Hassaan";
+let str2 = " Mahmood";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 1 + 2);
+console.log(1 + 1 + "2");
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(typeof +""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 3;
+console.log(num1, num2, num3);
+
+let counter = 0;
+counter++;
+++counter;
+console.log(counter); // 1
+console.log(counter++); // 0
+console.log(counter); // 1
+console.log(++counter); // 1
